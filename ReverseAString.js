a1f8ef7a36fb5dcaf5c1532935cdf0a3a9708a36
vtt01uk <vtt01uk@gmail.com>
2016-06-01 15:45:35 +0100

    First commit

---
+++ ReverseAString.js
@@ -0,0 +1,23 @@
+#Reverse the provided string.
+#
+#You may need to turn the string into an array before you can reverse it.
+#
+#Your result must be a string.
+#
+#Remember to use Read-Search-Ask if you get stuck. Write your own code.
+#
+#Here are some helpful links:
+#
+#Global String Object
+#String.split()
+#Array.reverse()
+#Array.join()
+
+function reverseString(str) {
+  str = str.split('').reverse().join('');
+  return str;
+}
+
+reverseString("hello"); //olleh
+reverseString("howdy"); //ydwoH
+reverseString("Greetings from Earth"); //htraE morf sgniteerG
