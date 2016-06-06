4f09e6f3804219db2f5470c4017eb6bb95841c0b
vtt01uk <vtt01uk@gmail.com>
2016-06-06 10:38:17 +0100

    Edited comments on previous algorithm solutions

--- FactorializeANumber.js
+++ FactorializeANumber.js
@@ -1,16 +1,16 @@
-#Return the factorial of the provided integer
-#
-#If the integer is represented with the letter n, a factorial is the product
-#of all positive integers less than or equal to n.
-#
-#Factorials are often represented with the shorthand notation n!
-#
-#For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
-#
-#Here are some helpful links:
-#
-# Arithmetic Operators
-#
+//Return the factorial of the provided integer
+//
+//If the integer is represented with the letter n, a factorial is the product
+//of all positive integers less than or equal to n.
+//
+//Factorials are often represented with the shorthand notation n!
+//
+//For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+//
+//Here are some helpful links:
+//
+// Arithmetic Operators
+//
 function factorialize(num) {
   var val = 1;
   for (num = num; num > 1; num--){
