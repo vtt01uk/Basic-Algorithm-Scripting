607df3d4e27724e40aa6e64461999658aa46fffd
vtt01uk <vtt01uk@gmail.com>
2016-07-01 10:15:48 +0100

    Added solution to 'Slasher Flick' challenge

---
+++ SlasherFlick.js
@@ -0,0 +1,23 @@
+//Return the remaining elements of an array after
+//chopping off n elements from the head.
+//
+//The head means the beginning of the array, or the zeroth index.
+//
+//Here are some helpful links:
+//
+//  Array.prototype.slice()
+//
+//  Array.prototype.splice()
+//
+function slasher(arr, howMany) {
+  // it doesn't always pay to be first
+  return arr.slice(howMany);
+}
+
+slasher([1, 2, 3], 2);                                      //[3]
+slasher([1, 2, 3], 0);                                      //[1, 2, 3]
+slasher([1, 2, 3], 9);                                      //[]
+slasher([1, 2, 3], 4);                                      //[]
+slasher(["burgers", "fries", "shake"], 1);                  //["fries", "shake"]
+slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4"], 5); //["cheese", 4]
+
